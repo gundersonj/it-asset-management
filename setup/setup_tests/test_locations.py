a41e007b739d615312fc6e5568dc43d1f1a1e16f
PromptlyCandid <gunderson.jeremy@gmail.com>
2022-07-29 11:39:01 -0500

    added LoginRequiredMixin to views. Updated tests.

--- setup/setup_tests/test_locations.py
+++ setup/setup_tests/test_locations.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, tag
 from django.urls import reverse, resolve
+from django.contrib.auth import get_user_model
 from setup.views import LocationListView, LocationCreateView, LocationUpdateView
 from setup.models import Location
 
@@ -24,27 +25,31 @@ class LocationModelTests(TestCase):
 @tag("listview")
 class LocationListViewTests(TestCase):
     def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="TestUser", email="test@email.com", password="testpass123",
+        )
+        self.login = self.client.login(username="TestUser", password="testpass123",)
         url = reverse("setup:location_list")
         self.response = self.client.get(url)
 
-    @tag("status_code")
-    def test_status_code(self):
+    @tag("status_code", "logged_in")
+    def test_status_code_for_logged_in_user(self):
         self.assertEqual(self.response.status_code, 200)
 
-    @tag("template")
-    def test_template(self):
+    @tag("template", "logged_in")
+    def test_template_for_logged_in_user(self):
         self.assertTemplateUsed(self.response, "setup/locations/location_list.html")
 
-    @tag("correct_html")
-    def test_contains_correct_html_code(self):
+    @tag("correct_html", "logged_in")
+    def test_contains_correct_html_code_for_logged_in_user(self):
         self.assertContains(self.response, "Locations", status_code=200, html=False)
 
-    @tag("incorrect_html")
-    def test_does_not_contain_incorrect_html(self):
+    @tag("incorrect_html", "logged_in")
+    def test_does_not_contain_incorrect_html_for_logged_in_user(self):
         self.assertNotContains(self.response, "Homepage")
 
-    @tag("resolve", "url")
-    def test_url_resolves_view(self):
+    @tag("resolve", "url", "logged_in")
+    def test_url_resolves_view_for_logged_in_user(self):
         view = resolve("/setup/locations/")
         self.assertEqual(
             view.func.__name__, LocationListView.as_view().__name__,
@@ -54,6 +59,10 @@ class LocationListViewTests(TestCase):
 @tag("location", "createview")
 class LocationCreateViewTests(TestCase):
     def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="TestUser", email="test@email.com", password="testpass123",
+        )
+        self.login = self.client.login(username="TestUser", password="testpass123",)
         url = reverse("setup:location_add")
         self.response = self.client.get(url)
 
@@ -67,24 +76,24 @@ class LocationCreateViewTests(TestCase):
         self.assertEqual(self.location.location_id, "002")
         self.assertEqual(self.location.location_name, "Second Loc")
 
-    @tag("status_code")
-    def test_status_code(self):
+    @tag("status_code", "logged_in")
+    def test_status_code_for_logged_in_user(self):
         self.assertEqual(self.response.status_code, 200)
 
-    @tag("template")
-    def test_template(self):
+    @tag("template", "logged_in")
+    def test_template_for_logged_in_user(self):
         self.assertTemplateUsed(self.response, "setup/locations/location_add.html")
 
-    @tag("correct_html")
-    def test_contains_correct_html_code(self):
+    @tag("correct_html", "logged_in")
+    def test_contains_correct_html_code_for_logged_in_user(self):
         self.assertContains(self.response, "Save Location")
 
-    @tag("incorrect_html")
-    def test_does_not_contain_incorrect_html_code(self):
+    @tag("incorrect_html", "logged_in")
+    def test_does_not_contain_incorrect_html_code_for_logged_in_user(self):
         self.assertNotContains(self.response, "Homepage")
 
-    @tag("resolve", "url")
-    def test_url_resolves_location_create_view(self):
+    @tag("resolve", "url", "logged_in")
+    def test_url_resolves_location_create_view_for_logged_in_user(self):
         view = resolve("/setup/locations/add/")
         self.assertEqual(
             view.func.__name__, LocationCreateView.as_view().__name__,
@@ -94,6 +103,10 @@ class LocationCreateViewTests(TestCase):
 @tag("updateview")
 class LocationUpdateViewTests(TestCase):
     def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="TestUser", email="test@email.com", password="testpass123",
+        )
+        self.login = self.client.login(username="TestUser", password="testpass123",)
         self.new_location = Location.objects.create(
             location_id="002", location_name="2nd Location",
         )
@@ -104,12 +117,12 @@ class LocationUpdateViewTests(TestCase):
             {"location_id": "002", "location_name": "2nd Test Location",},
         )
 
-    @tag("status_code")
-    def test_status_code(self):
+    @tag("status_code", "logged_in")
+    def test_status_code_for_logged_in_user(self):
         self.assertEqual(self.response.status_code, 302)
 
-    @tag("resolve", "url")
-    def test_url_resolves_view(self):
+    @tag("resolve", "url", "logged_in")
+    def test_url_resolves_view_for_logged_in_user(self):
         view = resolve("/setup/locations/002/edit/")
         self.assertEqual(
             view.func.__name__, LocationUpdateView.as_view().__name__,
--- setup/views.py
+++ setup/views.py
@@ -1,4 +1,5 @@
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView
 
@@ -6,19 +7,19 @@ from .models import Location
 from .forms import LocationForm
 
 
-class LocationListView(ListView):
+class LocationListView(LoginRequiredMixin, ListView):
     model = Location
     template_name = "setup/locations/location_list.html"
     context_object_name = "location_list"
 
 
-class LocationCreateView(CreateView):
+class LocationCreateView(LoginRequiredMixin, CreateView):
     form_class = LocationForm
     template_name = "setup/locations/location_add.html"
     success_url = reverse_lazy("setup:location_list")
 
 
-class LocationUpdateView(UpdateView):
+class LocationUpdateView(LoginRequiredMixin, UpdateView):
     model = Location
     form_class = LocationForm
     template_name = "setup/locations/location_edit.html"
